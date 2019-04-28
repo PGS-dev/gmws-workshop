71b998982fbb0227840071cefddab71b2b1899e3
izakrzewska <iga_zakrzewska@wp.pl>
2019-04-28 15:28:07 +0100

    add web workers & cache files

---
+++ Meeting II - Cache/sw.js
@@ -0,0 +1,21 @@
+
+
+if ('serviceWorker' in navigator) {
+    console.log('Service workers supported!');
+}
+
+
+self.addEventListener('install', function(event) {
+
+});
+
+
+self.addEventListener('fetch', function(event) {
+
+});
+
+
+self.addEventListener('activate', function(event) {
+
+
+});
---
+++ Meeting IV - Web Worker/index.js
@@ -0,0 +1,12 @@
+const first = document.querySelector('#number1');
+const second = document.querySelector('#number2');
+const result = document.querySelector('.result');
+
+if (window.Worker) {
+	// create worker
+	// send data to the worker
+	// receive data from worker
+
+} else {
+	console.log('MAIN: web workers are not supported in your browser');
+}
